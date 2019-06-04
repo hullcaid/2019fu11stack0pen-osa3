f38a08eb6e63f8523c161d4fe0f7217af8999210
Jussi Leinonen <hullcaid@gmail.com>
2019-06-04 21:45:31 +0300

    Tehtävä 3.1

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+let persons = [
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		Number: '045-1236543',
+	},
+	{
+		id: 2,
+		name: 'Arto Järvinen',
+		Number: '041-21423123',
+	},
+	{
+		id: 3,
+		name: 'Lea Kutvonen',
+		Number: '040-4323234',
+	},
+	{
+		id: 4,
+		name: 'Martti Tienari',
+		Number: '09-784232',
+	},
+]
+
+app.get('/API/persons', (request, response) => {
+	console.log('request received, sending')
+	response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT)
+	console.log(`server running, port: ${PORT}`);
