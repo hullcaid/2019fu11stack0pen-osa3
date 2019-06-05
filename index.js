2459eb6ea9b83b6d4006848efeaeb3ca09d4f0ac
Jussi Leinonen <hullcaid@gmail.com>
2019-06-05 21:47:56 +0300

    Tehtävä 3.4

--- index.js
+++ index.js
@@ -27,24 +27,54 @@ let persons = [
 	},
 ]
 
+//Function to return the whole list of contacts
 app.get('/api/persons', (request, response) => {
+	//Sends the persons array as json
 	response.json(persons);
 });
 
+//Function to return the status of the database
 app.get('/info', (request, response) => {
+	//Sends a infostring
 	response.send(`<p>Puhelinluettelossa ${persons.length} henkilön tiedot<br>${new Date()}`);
 });
 
+//Function to return single contact. If the contact does not exists, returns 404
 app.get('/api/persons/:id', (request, response) => {
+	//Gets the id from the request
 	const id = Number(request.params.id);
+	//Finds the person object from persons array if it exists
 	const person = persons.find(person => person.id === id);
 
 	if (person) {
+		//If the object with correct id is found, sends it as json
 		response.json(person);
 	} else {
-		response.status(404).end()
-	}
-})
+		//If the object was not found, sends 404
+		response.status(404).end();
+	};
+});
+
+//Function to remove contacts from the database
+app.delete('/api/persons/:id', (request, response) => {
+	//Get id from request and transform it to Number
+	const id = Number(request.params.id);
+	//Save the length of persons array before trying to remove the requested object
+	const oldLength = persons.length;
+	console.log(oldLength)
+	//Filter the objects without the requested id from the persons array and save the result to persons 
+	persons = persons.filter(person => person.id !== id);
+	console.log(oldLength, persons.length)
+	//Return code 204
+	if (persons.length !== oldLength) {
+		//if the array legth changed, removal was succesful, return 204
+		response.status(204).end();
+	} else {
+		//If the array is the same length, removal did not succeed, return 404
+		response.status(404).end();
+	};
+	
+});
 
 const PORT = 3001;
 app.listen(PORT)
